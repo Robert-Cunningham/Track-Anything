1fb63fd884490814752e5633681e1253cb8974ce
Robert Cunningham <robertvcunningham@gmail.com>
2023-08-01 01:36:26 -0700

    remove many spurious requirement

--- tracker/base_tracker.py
+++ tracker/base_tracker.py
@@ -3,6 +3,7 @@ import os
 import glob
 import numpy as np
 from PIL import Image
+
 # import for base_tracker
 import torch
 import yaml
@@ -16,7 +17,7 @@ from tracker.util.range_transform import im_normalization
 from tools.painter import mask_painter
 from tools.base_segmenter import BaseSegmenter
 from torchvision.transforms import Resize
-import progressbar
+from tqdm import tqdm
 
 
 class BaseTracker:
@@ -26,19 +27,21 @@ class BaseTracker:
         xmem_checkpoint: checkpoint of XMem model
         """
         # load configurations
-        with open("/home/robert/Documents/sd-hazel-2/Track-Anything/tracker/config/config.yaml", 'r') as stream: 
-            config = yaml.safe_load(stream) 
+        with open("/home/robert/Documents/sd-hazel-2/Track-Anything/tracker/config/config.yaml", "r") as stream:
+            config = yaml.safe_load(stream)
         # initialise XMem
         network = XMem(config, xmem_checkpoint).to(device).eval()
         # initialise IncerenceCore
         self.tracker = InferenceCore(network, config)
         # data transformation
-        self.im_transform = transforms.Compose([
-            transforms.ToTensor(),
-            im_normalization,
-        ])
+        self.im_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                im_normalization,
+            ]
+        )
         self.device = device
-        
+
         # changable properties
         self.mapper = MaskMapper()
         self.initialised = False
@@ -52,13 +55,12 @@ class BaseTracker:
         # mask transform is applied AFTER mapper, so we need to post-process it in eval.py
         h, w = mask.shape[-2:]
         min_hw = min(h, w)
-        return F.interpolate(mask, (int(h/min_hw*self.size), int(w/min_hw*self.size)), 
-                    mode='nearest')
+        return F.interpolate(mask, (int(h / min_hw * self.size), int(w / min_hw * self.size)), mode="nearest")
 
     @torch.no_grad()
     def track(self, frame, first_frame_annotation=None):
         """
-        Input: 
+        Input:
         frames: numpy arrays (H, W, 3)
         logit: numpy array (H, W), logit
 
@@ -68,7 +70,7 @@ class BaseTracker:
         painted_image: numpy array (H, W, 3)
         """
 
-        if first_frame_annotation is not None:   # first frame mask
+        if first_frame_annotation is not None:  # first frame mask
             # initialisation
             mask, labels = self.mapper.convert_mask(first_frame_annotation)
             mask = torch.Tensor(mask).to(self.device)
@@ -79,27 +81,27 @@ class BaseTracker:
         # prepare inputs
         frame_tensor = self.im_transform(frame).to(self.device)
         # track one frame
-        probs, _ = self.tracker.step(frame_tensor, mask, labels)   # logits 2 (bg fg) H W
+        probs, _ = self.tracker.step(frame_tensor, mask, labels)  # logits 2 (bg fg) H W
         # # refine
         # if first_frame_annotation is None:
-        #     out_mask = self.sam_refinement(frame, logits[1], ti)    
+        #     out_mask = self.sam_refinement(frame, logits[1], ti)
 
         # convert to mask
         out_mask = torch.argmax(probs, dim=0)
         out_mask = (out_mask.detach().cpu().numpy()).astype(np.uint8)
 
         final_mask = np.zeros_like(out_mask)
-        
+
         # map back
         for k, v in self.mapper.remappings.items():
             final_mask[out_mask == v] = k
 
         num_objs = final_mask.max()
         painted_image = frame
-        for obj in range(1, num_objs+1):
-            if np.max(final_mask==obj) == 0:
+        for obj in range(1, num_objs + 1):
+            if np.max(final_mask == obj) == 0:
                 continue
-            painted_image = mask_painter(painted_image, (final_mask==obj).astype('uint8'), mask_color=obj+1)
+            painted_image = mask_painter(painted_image, (final_mask == obj).astype("uint8"), mask_color=obj + 1)
 
         # print(f'max memory allocated: {torch.cuda.max_memory_allocated()/(2**20)} MB')
 
@@ -112,14 +114,14 @@ class BaseTracker:
         """
         # convert to 1, 256, 256
         self.sam_model.set_image(frame)
-        mode = 'mask'
+        mode = "mask"
         logits = logits.unsqueeze(0)
         logits = self.resizer(logits).cpu().numpy()
-        prompts = {'mask_input': logits}    # 1 256 256
+        prompts = {"mask_input": logits}  # 1 256 256
         masks, scores, logits = self.sam_model.predict(prompts, mode, multimask=True)  # masks (n, h, w), scores (n,), logits (n, 256, 256)
-        painted_image = mask_painter(frame, masks[np.argmax(scores)].astype('uint8'), mask_alpha=0.8)
+        painted_image = mask_painter(frame, masks[np.argmax(scores)].astype("uint8"), mask_alpha=0.8)
         painted_image = Image.fromarray(painted_image)
-        painted_image.save(f'/ssd1/gaomingqi/refine/{ti:05d}.png')
+        painted_image.save(f"/ssd1/gaomingqi/refine/{ti:05d}.png")
         self.sam_model.reset_image()
 
     @torch.no_grad()
@@ -135,28 +137,28 @@ class BaseTracker:
 #   XMEM_checkpoint = '/ssd1/gaomingqi/checkpoints/XMem-s012.pth'
 ##  2/3) initialise Base Tracker
 #   tracker = BaseTracker(XMEM_checkpoint, device, None, device)    # leave an interface for sam model (currently set None)
-##  3/3) 
+##  3/3)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # video frames (take videos from DAVIS-2017 as examples)
-    video_path_list = glob.glob(os.path.join('/ssd1/gaomingqi/datasets/davis/JPEGImages/480p/horsejump-high', '*.jpg'))
+    video_path_list = glob.glob(os.path.join("/ssd1/gaomingqi/datasets/davis/JPEGImages/480p/horsejump-high", "*.jpg"))
     video_path_list.sort()
     # load frames
     frames = []
     for video_path in video_path_list:
-        frames.append(np.array(Image.open(video_path).convert('RGB')))
-    frames = np.stack(frames, 0)    # T, H, W, C
+        frames.append(np.array(Image.open(video_path).convert("RGB")))
+    frames = np.stack(frames, 0)  # T, H, W, C
     # load first frame annotation
-    first_frame_path = '/ssd1/gaomingqi/datasets/davis/Annotations/480p/horsejump-high/00000.png'
-    first_frame_annotation = np.array(Image.open(first_frame_path).convert('P'))    # H, W, C
+    first_frame_path = "/ssd1/gaomingqi/datasets/davis/Annotations/480p/horsejump-high/00000.png"
+    first_frame_annotation = np.array(Image.open(first_frame_path).convert("P"))  # H, W, C
 
     # ------------------------------------------------------------------------------------
     # how to use
     # ------------------------------------------------------------------------------------
     # 1/4: set checkpoint and device
-    device = 'cuda:2'
-    XMEM_checkpoint = '/ssd1/gaomingqi/checkpoints/XMem-s012.pth'
+    device = "cuda:2"
+    XMEM_checkpoint = "/ssd1/gaomingqi/checkpoints/XMem-s012.pth"
     # SAM_checkpoint= '/ssd1/gaomingqi/checkpoints/sam_vit_h_4b8939.pth'
     # model_type = 'vit_h'
     # ------------------------------------------------------------------------------------
@@ -169,7 +171,7 @@ if __name__ == '__main__':
     for ti, frame in enumerate(frames):
         if ti == 0:
             mask, prob, painted_frame = tracker.track(frame, first_frame_annotation)
-            # mask: 
+            # mask:
         else:
             mask, prob, painted_frame = tracker.track(frame)
         painted_frames.append(painted_frame)
@@ -179,15 +181,15 @@ if __name__ == '__main__':
     # ----------------------------------------------
     # end
     # ----------------------------------------------
-    print(f'max memory allocated: {torch.cuda.max_memory_allocated()/(2**20)} MB')
+    print(f"max memory allocated: {torch.cuda.max_memory_allocated()/(2**20)} MB")
     # set saving path
-    save_path = '/ssd1/gaomingqi/results/TAM/blackswan'
+    save_path = "/ssd1/gaomingqi/results/TAM/blackswan"
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     # save
-    for painted_frame in progressbar.progressbar(painted_frames):
+    for painted_frame in tqdm(painted_frames):
         painted_frame = Image.fromarray(painted_frame)
-        painted_frame.save(f'{save_path}/{ti:05d}.png')
+        painted_frame.save(f"{save_path}/{ti:05d}.png")
 
     # tracker.clear_memory()
     # for ti, frame in enumerate(frames):
