d0b7a47748a52a204c1c4fec3e1c62e701a0e6b7
Robert Cunningham <robertvcunningham@gmail.com>
2023-10-03 14:29:48 -0700

    add config file to package

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import find_packages, setup
 
-setup(name="track_anything", version="0.3.0", description="Track Anything, Refactored", packages=find_packages(), install_requires=["numpy", "opencv-python", "pyyaml", "tqdm"])
+setup(
+    name="track_anything",
+    version="0.3.0",
+    description="Track Anything, Refactored",
+    packages=find_packages(),
+    install_requires=["numpy", "opencv-python", "pyyaml", "tqdm"],
+    package_data={"track_anything": ["xmem/config/config.yaml"]},
+)
